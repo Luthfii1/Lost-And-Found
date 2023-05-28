ddb576236a85758c741346da1ce2aca32183a8bb
Luthfii1 <luthfi.misbachul@ui.ac.id>
2023-05-28 23:18:24 +0700

    Fix opacity when clicked in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import {MdHome, MdSearch, MdOutlineNotificationsActive, } from 'react-icons/md'
-import {TbMessages} from 'react-icons/tb'
+import React, { useState } from 'react';
+import { MdHome, MdSearch, MdOutlineNotificationsActive } from 'react-icons/md';
+import { TbMessages } from 'react-icons/tb';
 
 const Footer = () => {
+  const [activeButton, setActiveButton] = useState('');
+
+  const handleButtonClick = (button) => {
+    setActiveButton(button);
+  };
 
   return (
-    // Creat a footer with home, search, notification, and message icons 100% opacity if selected, 50% opacity if not selected
-    // The footer should be fixed to the bottom of the page
-    // The footer should be black with white text
     <div className='fixed bottom-0 left-0 w-full rounded-t-2xl sm:hidden h-16 bg-black text-white flex justify-around items-center'>
-        <MdHome className='h-10 w-10 ' />
-        <MdSearch className='h-10 w-10' />
-        <MdOutlineNotificationsActive className='h-10 w-10' />
-        <TbMessages className='h-10 w-10' />
+      <MdHome
+        className={`h-10 w-10 ${activeButton === 'Home' ? 'opacity-100' : 'opacity-50'}`}
+        onClick={() => handleButtonClick('Home')}
+      />
+      <MdSearch
+        className={`h-10 w-10 ${activeButton === 'Search' ? 'opacity-100' : 'opacity-50'}`}
+        onClick={() => handleButtonClick('Search')}
+      />
+      <MdOutlineNotificationsActive
+        className={`h-10 w-10 ${activeButton === 'Notifications' ? 'opacity-100' : 'opacity-50'}`}
+        onClick={() => handleButtonClick('Notifications')}
+      />
+      <TbMessages
+        className={`h-10 w-10 ${activeButton === 'Messages' ? 'opacity-100' : 'opacity-50'}`}
+        onClick={() => handleButtonClick('Messages')}
+      />
     </div>
+  );
+};
 
-  )
-}
-
-export default Footer
+export default Footer;
